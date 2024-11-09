8aa295bb82e1362f83302f95152be708c1950776
umarz14 <martinezuriel2016@gmail.com>
2024-11-09 11:43:34 -0800

    update home

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,11 +1,22 @@
+"use client"
 import React from "react";
+import { WavyBackground } from "./ui/wavy-background";
 
 const Hero = () => {
     return (
-        <section id="hero" className="flex flex-col items-center justify-center min-h-screen bg-green-900 text-green-100 pt-20">
-        <h1 className="text-5xl font-bold">Hello, I'm [Your Name]</h1>
-        <p className="text-2xl mt-4">Aspiring Software Engineer | Full Stack Developer</p>
-      </section>
+      //   <section id="hero" className="flex flex-col items-center justify-center min-h-screen bg-green-900 text-green-100 pt-20">
+      //   <h1 className="text-5xl font-bold">Hello, I'm [Your Name]</h1>
+      //   <p className="text-2xl mt-4">Aspiring Software Engineer | Full Stack Developer</p>
+      // </section>
+      <WavyBackground className="max-w-max mx-auto pb-40">
+      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        Hello, I'm Uriel
+      </p>
+      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+        Aspiring Software Engineer | Full Stack Developer
+      </p>
+    </WavyBackground>
+      
     );
 };
 
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 left-0 w-full bg-green-700 text-green-100 py-4 shadow-md z-10">
+        <nav className="fixed top-0 left-0 w-full bg-green-900 text-green-100 py-4 shadow-md z-10">
         <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
             <h1 className="text-2xl font-bold">My Portfolio</h1>
             <div className="space-x-6">
